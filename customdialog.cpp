3d89372b7eb968dec14eba45f6629195f254b0ee
Curtis Bright <cbright@uwaterloo.ca>
2012-02-07 05:43:43 -0500

    fix tab ordering in custom dialog

--- customdialog.cpp
+++ customdialog.cpp
@@ -11,7 +11,7 @@ CustomDialog::CustomDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, wxT("C
 	wxTextCtrl* HeightInput = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
 	CustomSizer->Add(HeightInput, 0, wxALL, 5);
 	
-	wxButton* OKButton = new wxButton(this, ID_OK, wxT("OK"), wxDefaultPosition, wxDefaultSize, 0);
+	wxButton* OKButton = new wxButton(this, wxID_OK, wxT("OK"), wxDefaultPosition, wxDefaultSize, 0);
 	CustomSizer->Add(OKButton, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
 	
 	wxStaticText* WidthText = new wxStaticText(this, wxID_ANY, wxT("Width:"), wxDefaultPosition, wxDefaultSize, 0);
@@ -19,9 +19,10 @@ CustomDialog::CustomDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, wxT("C
 	CustomSizer->Add(WidthText, 0, wxALL, 5);
 	
 	wxTextCtrl* WidthInput = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
+	WidthInput->MoveAfterInTabOrder(HeightInput);
 	CustomSizer->Add(WidthInput, 0, wxALL, 5);
 	
-	wxButton* CancelButton = new wxButton(this, ID_CANCEL, wxT("Cancel"), wxDefaultPosition, wxDefaultSize, 0);
+	wxButton* CancelButton = new wxButton(this, wxID_CANCEL, wxT("Cancel"), wxDefaultPosition, wxDefaultSize, 0);
 	CustomSizer->Add(CancelButton, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
 	
 	wxStaticText* MinesText = new wxStaticText(this, wxID_ANY, wxT("Mines:"), wxDefaultPosition, wxDefaultSize, 0);
@@ -29,6 +30,7 @@ CustomDialog::CustomDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, wxT("C
 	CustomSizer->Add(MinesText, 0, wxALL, 5);
 	
 	wxTextCtrl* MinesInput = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
+	MinesInput->MoveAfterInTabOrder(WidthInput);
 	CustomSizer->Add(MinesInput, 0, wxALL, 5);
 	
 	this->SetSizer(CustomSizer);
