58cdd50a31895084149c17837ff1456187abe4f5
Curtis Bright <cbright@uwaterloo.ca>
2012-03-03 09:57:39 -0500

    better formatting for dialogs

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -5,43 +5,34 @@
 #include "graphics/winmine_ico.xpm"
 
 AboutDialog::AboutDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, wxT("About"))
-{
-	wxBoxSizer* aboutSizer = new wxBoxSizer(wxHORIZONTAL);
-	
-	wxStaticBitmap* icon = new wxStaticBitmap(this, wxID_ANY, wxIcon(winmine_ico), wxDefaultPosition, wxDefaultSize, 0);
-	aboutSizer->Add(icon, 0, wxALL, 5);
-	
+{	wxBoxSizer* aboutSizer = new wxBoxSizer(wxHORIZONTAL);
+	wxStaticBitmap* icon = new wxStaticBitmap(this, wxID_ANY, wxIcon(winmine_ico));
 	wxBoxSizer* descriptionSizer = new wxBoxSizer(wxVERTICAL);
-	
-	wxStaticText* titleText = new wxStaticText(this, wxID_ANY, wxT("Minesweeper X"), wxDefaultPosition, wxDefaultSize, 0);
-	titleText->SetFont(wxFont(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+	wxStaticText* titleText = new wxStaticText(this, wxID_ANY, wxT("Minesweeper X"));
+	titleText->SetFont(wxFont(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, wxT("Tahoma")));
+	wxStaticText* versionText = new wxStaticText(this, wxID_ANY, wxT("Version 2.0"));
+	wxStaticText* authorText = new wxStaticText(this, wxID_ANY, wxT("by Curtis Bright"));
+	wxStaticLine* separator = new wxStaticLine(this);
+	wxStaticText* originText = new wxStaticText(this, wxID_ANY, wxT("Based on the Microsoft game by"));
+	wxStaticText* originText2 = new wxStaticText(this, wxID_ANY, wxT("Robert Donner and Curt Johnson."));
+	wxStaticText* descriptionText = new wxStaticText(this, wxID_ANY, wxT("Minesweeper X is free software, and was"));
+	wxStaticText* descriptionText2 = new wxStaticText(this, wxID_ANY, wxT("developed using the wxWidgets library."));
+	wxHyperlinkCtrl* websiteText = new wxHyperlinkCtrl(this, wxID_ANY, wxT("http://www.curtisbright.com/msx/"), wxT("http://www.curtisbright.com/msx/"));
+	wxButton* OKButton = new wxButton(this, wxID_CANCEL, wxT("OK"));
+
 	descriptionSizer->Add(titleText, 0, wxALIGN_TOP|wxLEFT|wxTOP, 5);
-	
-	wxStaticText* versionText = new wxStaticText(this, wxID_ANY, wxT("Version 2.0"), wxDefaultPosition, wxDefaultSize, 0);
 	descriptionSizer->Add(versionText, 0, wxLEFT, 5);
-	
-	wxStaticText* authorText = new wxStaticText(this, wxID_ANY, wxT("by Curtis Bright"), wxDefaultPosition, wxDefaultSize, 0);
 	descriptionSizer->Add(authorText, 0, wxLEFT, 5);
-	
-	wxStaticLine* separator = new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL);
-	descriptionSizer->Add(separator, 0, wxEXPAND | wxALL, 5);
-	
-	wxStaticText* originText = new wxStaticText(this, wxID_ANY, wxT("Based on the Microsoft game by Robert Donner and Curt Johnson."), wxDefaultPosition, wxDefaultSize, 0);
-	originText->Wrap(300);
+	descriptionSizer->Add(separator, 0, wxEXPAND|wxALL, 5);
 	descriptionSizer->Add(originText, 0, wxLEFT, 5);
-	
-	wxStaticText* descriptionText = new wxStaticText(this, wxID_ANY, wxT("Minesweeper X is free software, and was developed using the wxWidgets library."), wxDefaultPosition, wxDefaultSize, 0);
-	descriptionText->Wrap(300);
+	descriptionSizer->Add(originText2, 0, wxLEFT, 5);
 	descriptionSizer->Add(descriptionText, 0, wxLEFT|wxTOP|wxRIGHT, 5);
-	
-	wxHyperlinkCtrl* websiteText = new wxHyperlinkCtrl(this, wxID_ANY, wxT("http://www.curtisbright.com/msx/"), wxT("http://www.curtisbright.com/msx/"), wxDefaultPosition, wxDefaultSize, wxHL_DEFAULT_STYLE);
+	descriptionSizer->Add(descriptionText2, 0, wxLEFT|wxRIGHT, 5);
 	descriptionSizer->Add(websiteText, 0, wxBOTTOM|wxLEFT|wxTOP, 5);
-	
-	wxButton* OKButton = new wxButton(this, wxID_CANCEL, wxT("OK"), wxDefaultPosition, wxDefaultSize, 0);
 	descriptionSizer->Add(OKButton, 0, wxALIGN_RIGHT|wxALL, 5);
 	
+	aboutSizer->Add(icon, 0, wxALL, 5);
 	aboutSizer->Add(descriptionSizer, 1, wxBOTTOM|wxEXPAND|wxRIGHT, 5);
-	
 	this->SetSizer(aboutSizer);
 	aboutSizer->Fit(this);
 	
--- customdialog.cpp
+++ customdialog.cpp
@@ -2,35 +2,26 @@
 #include "customdialog.h"
 
 CustomDialog::CustomDialog(wxWindow* parent, int height, int width, int mines) : wxDialog(parent, wxID_ANY, wxT("Custom Field"))
-{
-	wxGridSizer* customSizer = new wxFlexGridSizer(3, 3, 0, 0);
-	
-	wxStaticText* heightText = new wxStaticText(this, wxID_ANY, wxT("Height:"), wxDefaultPosition, wxSize(-1,-1), 0);
-	customSizer->Add(heightText, 1, wxALL, 5);
-	
-	heightInput = new wxTextCtrl(this, wxID_ANY, wxString::Format(wxT("%i"), height), wxDefaultPosition, wxDefaultSize, 0);
-	customSizer->Add(heightInput, 0, wxALL, 5);
-	
-	wxButton* OKButton = new wxButton(this, wxID_OK, wxT("OK"), wxDefaultPosition, wxDefaultSize, 0);
-	customSizer->Add(OKButton, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
-	
-	wxStaticText* widthText = new wxStaticText(this, wxID_ANY, wxT("Width:"), wxDefaultPosition, wxDefaultSize, 0);
-	customSizer->Add(widthText, 0, wxALL, 5);
-	
-	widthInput = new wxTextCtrl(this, wxID_ANY, wxString::Format(wxT("%i"), width), wxDefaultPosition, wxDefaultSize, 0);
+{	wxGridSizer* customSizer = new wxFlexGridSizer(3, 3, 0, 0);
+	wxStaticText* heightText = new wxStaticText(this, wxID_ANY, wxT("Height:"));
+	heightInput = new wxTextCtrl(this, wxID_ANY, wxString::Format(wxT("%i"), height), wxDefaultPosition, wxSize(50, -1));
+	wxButton* OKButton = new wxButton(this, wxID_OK, wxT("OK"));
+	wxStaticText* widthText = new wxStaticText(this, wxID_ANY, wxT("Width:"));
+	widthInput = new wxTextCtrl(this, wxID_ANY, wxString::Format(wxT("%i"), width), wxDefaultPosition, wxSize(50, -1));
 	widthInput->MoveAfterInTabOrder(heightInput);
-	customSizer->Add(widthInput, 0, wxALL, 5);
-	
-	wxButton* cancelButton = new wxButton(this, wxID_CANCEL, wxT("Cancel"), wxDefaultPosition, wxDefaultSize, 0);
-	customSizer->Add(cancelButton, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
-	
-	wxStaticText* minesText = new wxStaticText(this, wxID_ANY, wxT("Mines:"), wxDefaultPosition, wxDefaultSize, 0);
-	customSizer->Add(minesText, 0, wxALL, 5);
-	
-	minesInput = new wxTextCtrl(this, wxID_ANY, wxString::Format(wxT("%i"), mines), wxDefaultPosition, wxDefaultSize, 0);
+	wxButton* cancelButton = new wxButton(this, wxID_CANCEL, wxT("Cancel"));
+	wxStaticText* minesText = new wxStaticText(this, wxID_ANY, wxT("Mines:"));
+	minesInput = new wxTextCtrl(this, wxID_ANY, wxString::Format(wxT("%i"), mines), wxDefaultPosition, wxSize(50, -1));
 	minesInput->MoveAfterInTabOrder(widthInput);
-	customSizer->Add(minesInput, 0, wxALL, 5);
 	
+	customSizer->Add(heightText, 1, wxALL, 5);
+	customSizer->Add(heightInput, 0, wxALL, 5);
+	customSizer->Add(OKButton, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+	customSizer->Add(widthText, 0, wxALL, 5);
+	customSizer->Add(widthInput, 0, wxALL, 5);
+	customSizer->Add(cancelButton, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+	customSizer->Add(minesText, 0, wxALL, 5);
+	customSizer->Add(minesInput, 0, wxALL, 5);
 	this->SetSizer(customSizer);
 	customSizer->Fit(this);
 }
